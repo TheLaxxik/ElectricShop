86be17be8ab84ddf57189a1a571a5815e911e590
Ivan Dvorovy <ivandvorovy@gmail.com>
2026-05-27 22:54:34 +0200

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://electricshop.esticode.sk',
   vite: {
     plugins: [tailwindcss()]
   },
